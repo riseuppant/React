5b10a1051a34e9ee1d88eafc049ec50e299d3c15
riseuppant <hardikbatwal1505@gmail.com>
2025-07-14 02:02:11 +0530

    Done with basics of react and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai and Code</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur React with Vite | HB</h1>
+  )
+}
+
+export default App
