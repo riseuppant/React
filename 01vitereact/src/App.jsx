772e4a316e942553da562c504c59af679da22d82
riseuppant <hardikbatwal1505@gmail.com>
2025-07-17 01:00:54 +0530

    Created Own Library with some more changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,17 @@
 import Jimmy from "./index2" 
 function App() {
+  const username="Hardik"
+
   return (
     <>
     <Jimmy/>
-    <h1>Chai aur react</h1>
+    <h1>Chai aur react {username}</h1>
     <h2>Chai aur react</h2>
     <p>test para</p>
     </>
   )
 }
+
+//How would you import JS in this 
 
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 function MyApp(){
   return(
@@ -8,8 +9,35 @@ function MyApp(){
       </div>
   )
 }
+/*
+const ReactElement = {
+    type : 'a',
+    props: {
+        href : 'https://www.google.com',
+        target: '_blank'
+    },
+    children : 'Click me to visit google'
+}
+    */
+
+const createElement =React.createElement('a',{
+  href:'https://google.com',
+  target : '_blank'
+},"click me to visit Google")
+
+const anotherElement =( // Use this brackets as it is not an object
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MyApp />
-  </StrictMode>,
+  //Works in strict mode
+    //ReactElement----->Use this too 
+  //anotherElement---->Use this
+   //This now works as it internally gets converted to an object and then returned to dom from the custom render 
+  //  createElement //This works because tum react library use kar skte ho
+   //Thode changes allowed h but not complete freedom
+  <App/>
+   
+  //Try using Capital 
 )
