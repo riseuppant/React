b9ee02cf6258d2fc7926ac0fac06c6c6f6f23ec4
riseuppant <hardikbatwal1505@gmail.com>
2025-07-19 01:33:40 +0530

    PwdGen

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numbers,setNumbers] = useState(false);
+  const [char,setChar]=useState(false)
+  const [Password,setPassword]=useState("")
+
+  const passwordGenerator=()=>{
+    
+  }
+  return (
+
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
